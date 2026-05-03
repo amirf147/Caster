69e6e8bc4097d08fb0d54818d604bd553b0f399f
Amir Farhadi <amirf147@gmail.com>
2026-05-03 18:52:47 +0300

    fix: pin python execution to version 3.10 in installation and runtime batch scripts

--- Install_Caster_Kaldi.bat
+++ Install_Caster_Kaldi.bat
@@ -2,11 +2,11 @@
 set currentpath=%~dp0
 echo Installation path: %currentpath%
 echo Using this python/pip:
-python -m pip -V
+py -3.10 -m pip -V
 
 echo Installing Caster Dependencies
-py -m pip install -r "%currentpath%requirements.txt"
-py -m pip install dragonfly2[kaldi]
+py -3.10 -m pip install -r "%currentpath%requirements.txt"
+py -3.10 -m pip install dragonfly2[kaldi]
 
 echo Remember: Manually install kaldi a model. 
 echo See Caster kaldi install instructions on ReadTheDocs.
--- Run_Caster_Kaldi.bat
+++ Run_Caster_Kaldi.bat
@@ -4,6 +4,6 @@ echo Running Kaldi from Dragonfly CLI
 set currentpath=%~dp0
 
 TITLE Caster: Status Window
-py -m dragonfly load _*.py --engine kaldi --no-recobs-messages --engine-options "model_dir=kaldi_model, vad_padding_end_ms=300"
+py -3.10 -m dragonfly load _*.py --engine kaldi --no-recobs-messages --engine-options "model_dir=kaldi_model, vad_padding_end_ms=300"
 
 pause 1
