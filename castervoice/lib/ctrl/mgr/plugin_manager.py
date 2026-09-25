134bf46d40e759742353b0ac710ed37cb3041e47
Amir Farhadi <amirf147@gmail.com>
2026-09-25 15:46:31 +0300

    style(plugins): align source files with upstream repository licensing conventions
    Remove redundant individual copyright headers and alternate SPDX-License-Identifier declarations from plugin subsystem modules. Ensure all new files conform uniformly to Caster's existing LGPL-3.0 repository licensing without mixed file-level headers.
    
    - Remove individual copyright and SPDX lines from castervoice/lib/plugin.py and castervoice/plugins/__init__.py
    - Remove file-level licensing declarations from castervoice/lib/ctrl/mgr/plugin_manager.py and plugin_support.py
    - Remove file-level licensing declarations from castervoice/bin/plugin_cli.py
    - Align unit test headers in tests/bin/test_plugin_cli.py and tests/lib/ctrl/mgr/test_plugin_manager.py

--- castervoice/lib/ctrl/mgr/plugin_manager.py
+++ castervoice/lib/ctrl/mgr/plugin_manager.py
@@ -1,6 +1,3 @@
-# SPDX-License-Identifier: Apache-2.0
-# Copyright (c) 2026 Amir Farhadi
-
 """
 Caster Plugin Manager
 
--- tests/bin/test_plugin_cli.py
+++ tests/bin/test_plugin_cli.py
@@ -1,6 +1,3 @@
-# SPDX-License-Identifier: Apache-2.0
-# Copyright (c) 2026 Amir Farhadi
-
 """
 Unit tests for Caster plugin_cli tool.
 """
